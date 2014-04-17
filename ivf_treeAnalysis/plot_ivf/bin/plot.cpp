772306bd6672085731aff5bc552ad1a8b516a602
Dominik Nowatschin <dominik.nowatschin@desy.de>
2014-04-17 14:56:27 +0200

    Changed plotting module for ivf_treeAnalysis

--- ivf_treeAnalysis/plot_ivf/bin/plot.cpp
+++ ivf_treeAnalysis/plot_ivf/bin/plot.cpp
@@ -7,33 +7,33 @@ using namespace std;
 using namespace ra;
 
 int main(){
-    shared_ptr<ProcessHistograms> ttbar(new ProcessHistogramsTFile("../ttbar.root", "ttbar"));
-    shared_ptr<ProcessHistograms> dy(new ProcessHistogramsTFile("../dy.root", "dy"));
-    shared_ptr<ProcessHistograms> data(new ProcessHistogramsTFile("../dmu.root", "data"));
+    shared_ptr<ProcessHistograms> ivf_processes(new ProcessHistogramsTFile("../rootfiles/ivf_hist_mcttbar_new_9_addhists_test.root", "ivf_processes"));
+//     shared_ptr<ProcessHistograms> dy(new ProcessHistogramsTFile("../dy.root", "dy"));
+//     shared_ptr<ProcessHistograms> data(new ProcessHistogramsTFile("../dmu.root", "data"));
     
     Formatters formatters;
-    formatters.add("*", SetLineColor(1));
-    formatters.add<SetFillColor>("ttbar:", 810)  ("dy:", 414);
-    formatters.add<SetLegends>("ttbar:", "t#bar{t}+Jets", "$\\ttbar$") ("dy:", "Z/#gamma*+Jets", "$Z/\\gamma^*$+Jets") ("data:", "Data");
-    formatters.add<SetOption>
-       ("*", "title_ur", "19.7 fb^{-1}, #sqrt{s}=8TeV")
-       ("*", "draw_ratio", "1")
-       ("*", "ytext", "events")
-       ("mll", "xtext", "m_{ll} [GeV]")
-       ("DR_ZB", "xtext", "#Delta R(Z,B)")
-       ("DR_BB", "xtext", "#Delta R(B,B)")
-       ("DR_VV", "xtext", "#Delta R(V,V)")
-       ("A_ZBB", "xtext", "A_{ZBB}")
-       ("met", "xtext", "E_{T}^{miss} [GeV]")
-       ("m_BB", "xtext", "m_{BB} [GeV]")
-       ("Beta", "xtext", "#eta_B")
-       ("Bpt", "xtext", "p_{T,B} [GeV]")
-       ("min_DR_ZB", "xtext", "min #Delta R(Z,B)")
-       ("DPhi_BB", "xtext", "#Delta #phi(B,B)");
-    formatters.add<RebinFactor>("mll", 4);
-    formatters.add<Ignore>("bcand2_mll/");
+//     formatters.add("*", SetLineColor(1));
+//     formatters.add<SetFillColor>("ttbar:", 810)  ("dy:", 414);
+//     formatters.add<SetLegends>("ttbar:", "t#bar{t}+Jets", "$\\ttbar$") ("dy:", "Z/#gamma*+Jets", "$Z/\\gamma^*$+Jets") ("data:", "Data");
+//     formatters.add<SetOption>
+//        ("*", "title_ur", "19.7 fb^{-1}, #sqrt{s}=8TeV")
+//        ("*", "draw_ratio", "1")
+//        ("*", "ytext", "events")
+//        ("mll", "xtext", "m_{ll} [GeV]")
+//        ("DR_ZB", "xtext", "#Delta R(Z,B)")
+//        ("DR_BB", "xtext", "#Delta R(B,B)")
+//        ("DR_VV", "xtext", "#Delta R(V,V)")
+//        ("A_ZBB", "xtext", "A_{ZBB}")
+//        ("met", "xtext", "E_{T}^{miss} [GeV]")
+//        ("m_BB", "xtext", "m_{BB} [GeV]")
+//        ("Beta", "xtext", "#eta_B")
+//        ("Bpt", "xtext", "p_{T,B} [GeV]")
+//        ("min_DR_ZB", "xtext", "min #Delta R(Z,B)")
+//        ("DPhi_BB", "xtext", "#Delta #phi(B,B)");
+//     formatters.add<RebinFactor>("mll", 4);
+//     formatters.add<Ignore>("bcand2_mll/");
     
-    Plotter p("eps", {ttbar, dy, data}, formatters);
+    Plotter p("pdf", {ivf_processes}, formatters);
     
     //std::map<identifier, double> factors;
     //factors["ttbar"] = 1.1;
