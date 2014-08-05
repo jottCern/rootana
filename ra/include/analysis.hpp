231f2be8e00b2f08577fc872caec9e2e4816d91c
Jochen Ott <jott@cern.ch>
2014-08-05 11:08:31 +0200

    Improve documentation of AnalysisModule

--- ra/include/analysis.hpp
+++ ra/include/analysis.hpp
@@ -7,14 +7,24 @@
 
 using boost::property_tree::ptree;
 
-
 namespace ra {
     
 /** \brief Abstract base class for all analysis modules
  *
- * Derive from this class and implement the 'begin_dataset' and 'process' methods. Make sure
- * to use the REGISTER_ANALYSIS_MODULE macro to make your module known to the plugin system.
- * Then name it in the configuration to execute it.
+ * To implement your own AnalysisModule class that can be used in the 'modules' section of the configuration file,
+ * you have to 
+ *  1. Create a new clas which derives from this class and provide a constructor taking the configuration (a 'const ptree &') as only argument
+ *  2. implement the pure virtual methods, i.e. \c begin_dataset and \c process
+ *  3. Use the REGISTER_ANALYSIS_MODULE macro at the global scope of your .cpp file to make your class known to the plugin system
+ * 
+ * These are the minimal steps required to use the module from the configuration.
+ * 
+ * For modules needing more control, there is also a \c begin_in_file method which is called whenever a new
+ * root file is opened.
+ * 
+ * Note for modules which are not safe to be executed in parallel (such as modules checking for duplicate
+ * events which can only be reliable if they see the whole dataset) should also override
+ * the 'is_parallel_safe' method to return \c false such that an error is raised eraly in the processing.
  */
 class AnalysisModule {
 public:
@@ -29,14 +39,14 @@ public:
      *  3. declaring which Event members to write to the output TTree(s) via 'out.declare_(event)_output'
      *  4. use 'in' to declare input variables and where to find them in the input TTree
      * 
-     * Note that typically, most modules only need 1. and 2.; for each data format, one can then write two AnalysisModules
-     * performing 3. and 4.
+     * Note that typically, most modules only need 1. and 2.; and let special AnalysisModules takes care of 3. and 4.,
+     * where usually only one is needed per TTree format.
      */
     virtual void begin_dataset(const s_dataset & dataset, InputManager & in, OutputManager & out) = 0;
     
     /** \brief Method called for each event in the dataset
      *
-     * Do the filling of histograms, calculating new variables, etc. here.
+     * Do the filling of histograms, calculate new variables for the event, etc.
      */
     virtual void process(Event & event) = 0;
     
@@ -46,16 +56,27 @@ public:
      * provided by the AnalysisModules via the InputManager. Before setting up the TTree, this
      * method is called to allow some special handling of information in the input file.
      * 
-     * NOTE: For simple analyses, it should not be necessary to override this, as all required information
-     * is read on an per-event basis. This method should only by used if it is necessary to access information
+     * NOTE: For simple analyses, it should not be necessary to override this method, as all required information
+     * is usually on a per-event basis and reading the event stream is handled by the framework (and by the \c InputManager, see
+     * \c begin_dataset). Therefore, this method should only by used if it is necessary to access information
      * in the input file that is *not* part of this per-event information.
      * While it is possible to access the input event tree, some care should be taken: after
-     * begin_file returns, the input tree will be set up according to the declarations provided to the InputManager which could
-     * replace connections (TTree::SetAddress) made here. Therefore, try to avoid touching the input event tree; if you do, do not
+     * \c begin_in_file returns and before the first \c process call is made, the framework sets up the input tree
+     * according to the declarations provided to the \c InputManager which might replace connections
+     * (TTree::SetAddress) made here. Therefore, try to avoid touching the input event tree; if you do, do not
      * expect that anything done here has any effect after this method returns.
      */
     virtual void begin_in_file(TFile & input_file){}
     
+    
+    /** \brief Return whether this AnalysisModule can be considered safe for parallel/distributed execution
+     * 
+     * This method is used by the framework for basic consistency checks: When executing in a parellel environment,
+     * an exception is raised before event processing unless all AnalysisModules return \c true for this method.
+     * 
+     * Note that the value returned here can depend on the configuration provided in the constructor, but it is called
+     * before \c begin_dataset and hence can not depend on any initialization done there.
+     */
     virtual bool is_parallel_safe() const { return true; }
 };
 
